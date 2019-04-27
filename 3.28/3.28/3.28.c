7233e5027d783da1e00b680ec4ba71b47cf6e0a7
zhloveyt <1397354769@qq.com>
2019-04-27 17:16:28 +0800

    日常练习

---
+++ 3.28/3.28/3.28.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int a=0;
+	int b=2;
+	if(a==1)
+		{if(b==2)
+			printf("haha\n");
+	}
+		else
+		{printf("hehe\n");}
+	system ("pause");
+
+		return 0;}
+//if循环语句注意括号使用。
+
