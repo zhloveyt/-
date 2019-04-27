ab145ac0b674cd3b79dbf3d53addd5c346741e78
zhloveyt <1397354769@qq.com>
2019-04-27 17:10:23 +0800

    日常练习

---
+++ 3.24日学习/3.24日学习/test.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	/*int i=0;
+	for(i=0;i<=100;i++)
+	{if(i%2==1)
+	{printf("%d\n",i);
+	     }
+	}*/
+	int day=0;
+	scanf("%d",&day);
+	switch(day)
+	{
+		case 1:
+			printf("星期一");
+		case 2:
+			printf("星期二");
+			brank;
+		case 3:
+			printf("星期三");
+			brank;
+		case 4:
+			printf("星期四");
+			brank;
+		case 5:
+			printf("星期五");
+		case 6:
+			printf("星期六");
+		case 7:
+			printf("星期日");
+			system("pause");
+	}
+	return 0;
+	}
+
