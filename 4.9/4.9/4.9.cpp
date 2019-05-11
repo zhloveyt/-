a1698917925aa0579cf9d545a870cb10ab68baeb
zhloveyt <1397354769@qq.com>
2019-05-11 23:06:23 +0800

    日常练习

---
+++ 4.9/4.9/4.9.cpp
@@ -0,0 +1,76 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main()
+//{
+//int a=24;
+//int b=16;
+//int c=0;
+//while(c=a%b)
+//{
+//a=b;
+//b=c;
+//}
+//printf("%d",b);
+//system ("pause");
+//return 0;
+//}
+//#include<math.h>
+// viod Swap (int x,int y)
+//{
+//	int temp=x;
+//	x=y;
+//	y=temp;
+//}
+//int main()
+//{
+//	int a=1;
+//	int b=2;
+//	int c=3;
+//	if(a<b)
+//	{
+//		 Swap(&a,&b);
+//	}
+//	if(a<c)
+//	{
+//		Swap(&a,&c);
+//		
+//	}
+//	if(b<c)
+//	{
+//		 Swap(&b,&c);
+//	}
+//	printf("%d%d%d",a b c);
+//	system("pause");
+//	return 0;
+// } 
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<math.h>
+//int main()
+//{
+//	int a=1;
+//	int b=2;
+//	int c=3;
+//	int tmp=0;
+//	if(a<b)
+//	{tmp=a;
+//	a=b;
+//	b=tmp; 
+//	}
+//	if(a<c)
+//	{tmp=a;
+//	a=c;
+//	c=tmp;
+//	}
+//	if(b<c)
+//	{
+//		 tmp=b;
+//		 b=c;
+//		 c=tmp;
+//	}
+//	printf("%d%d%d",a,b,c);
+//	system("pause");
+//	return 0;
+// } 
