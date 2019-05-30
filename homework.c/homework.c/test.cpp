5c12fb6f24479378f381d2000bb9dd97744217d6
zhloveyt <1397354769@qq.com>
2019-05-30 21:37:29 +0800

    练习和做题模板

---
+++ homework.c/homework.c/test.cpp
@@ -0,0 +1,71 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+
+}
+//int main()
+//{
+//int a=0;
+//int tmp=0;
+//int b=0;
+//scanf("%d",&a);
+//tmp=a^b;
+//printf("%d",tmp);
+//system("pause");
+//return 0;
+//}
+//int main()
+//{
+//int i=0;
+//int n=1;
+//int sum=1 ;
+//scanf("%d",&n);
+//for(i=1;i<=n;i++)
+//{
+//sum*=i;
+//}
+//printf("%d",sum);
+//system("pause");
+//return 0;
+//}
+//1.///同为单目右边亲，数组指针
+//2.//定义指针几级指针几个p一个*抵消一个    p**pppa=*ppb,a指针指向p
+//
+//int main()
+//{
+//for(cur=heas;cur;cur=cur->next）
+//{
+//
+//}
+//}
+//链表遍历：cur完成了对链表节点的遍历
+//3.for(i=a;i<=b;i++)
+//	数字遍历：i完成了[a,b]之间的遍历
+//4.for(i=n;i;i/=s)i%s
+//
+//数字遍历：i%s完成了数字n在s进制下的每一位遍历
+//5.for(i=0;str[i];i++)str[i]
+//字符串遍历：str[i]完成了对字符串str的遍历
+//6.for(p=v.begin();p!=v.end;p++) p
+//容器遍历：迭代器p完成了对容器v的遍历
+//7.求Sn=a+aa+aaa+aaaa+aaaaa的前5项和，其中a是一个数字
+//例如：2+22+222+2222+22222
+//1!+2!+3!+.....+n!
+//for(i=1；i<=n;i++）
+//{
+//sum*=i;
+//}
+//for(i=1；i<=n;i++）
+//{
+//tmp*=i;
+//sum+=tmp;
+//}
+//for(i=1；i<=5;i++）
+//{
+//tmp=tmp*10+a;
+//sum+=tmp;
+//}
+//
+//
